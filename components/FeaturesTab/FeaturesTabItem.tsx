6034167692b19f9485dff8c80e6123be5c0aaab3
Elikem <elikemowuraku@gmail.com>
2026-02-26 13:02:52 +0000

    Fixed small config issues in the packages and next.config. Also had to change configs for upgraded packages

--- components/FeaturesTab/FeaturesTabItem.tsx
+++ components/FeaturesTab/FeaturesTabItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FeatureTab } from "@/types/featureTab";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
